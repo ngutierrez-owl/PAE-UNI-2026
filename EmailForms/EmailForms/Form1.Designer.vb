360be8e1bc00ea9ccd49d1841fecfa35aac3f3ec
ACR-UBX-27U1 <ACR-UBX-27U1@SOPORTE-01>
2026-08-11 11:38:02 -0600

    Agregar archivos de proyecto.

---
+++ EmailForms/EmailForms/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCerrar = New Button()
+        btnEnviar = New Button()
+        txtPara = New TextBox()
+        txtAsunto = New TextBox()
+        txtCuerpo = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnCerrar
+        ' 
+        btnCerrar.Location = New Point(333, 223)
+        btnCerrar.Name = "btnCerrar"
+        btnCerrar.Size = New Size(67, 23)
+        btnCerrar.TabIndex = 0
+        btnCerrar.Text = "Cerrar"
+        btnCerrar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEnviar
+        ' 
+        btnEnviar.Location = New Point(263, 223)
+        btnEnviar.Name = "btnEnviar"
+        btnEnviar.Size = New Size(64, 22)
+        btnEnviar.TabIndex = 1
+        btnEnviar.Text = "Eviar"
+        btnEnviar.UseVisualStyleBackColor = True
+        ' 
+        ' txtPara
+        ' 
+        txtPara.Location = New Point(25, 34)
+        txtPara.Name = "txtPara"
+        txtPara.Size = New Size(160, 23)
+        txtPara.TabIndex = 2
+        ' 
+        ' txtAsunto
+        ' 
+        txtAsunto.Location = New Point(25, 63)
+        txtAsunto.Name = "txtAsunto"
+        txtAsunto.Size = New Size(160, 23)
+        txtAsunto.TabIndex = 3
+        ' 
+        ' txtCuerpo
+        ' 
+        txtCuerpo.Location = New Point(25, 105)
+        txtCuerpo.Name = "txtCuerpo"
+        txtCuerpo.Size = New Size(357, 23)
+        txtCuerpo.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(410, 262)
+        Controls.Add(txtCuerpo)
+        Controls.Add(txtAsunto)
+        Controls.Add(txtPara)
+        Controls.Add(btnEnviar)
+        Controls.Add(btnCerrar)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents btnEnviar As Button
+    Friend WithEvents txtPara As TextBox
+    Friend WithEvents txtAsunto As TextBox
+    Friend WithEvents txtCuerpo As TextBox
+
+End Class
---
+++ EmailForms/EmailForms/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Net
+Imports System.Net.Mail
+
+Public Class Form1
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        Dim resultado As DialogResult = MessageBox.Show(
+            "¿Desea cerrar la aplicación?",
+            "Confirmar",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question)
+
+        If resultado = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
+        ' Validaciones básicas
+        If String.IsNullOrWhiteSpace(txtAsunto.Text) Then
+            MessageBox.Show("Debe indicar al menos un destinatario.",
+                "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtAsunto.Focus()
+            Return
+        End If
+
+        If String.IsNullOrWhiteSpace(txtAsunto.Text) Then
+            MessageBox.Show("Debe indicar el asunto del correo.",
+                "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtAsunto.Focus()
+            Return
+        End If
+
+        Try
+            Me.Cursor = Cursors.WaitCursor
+            btnEnviar.Enabled = False
+
+            Using mensaje As New MailMessage()
+                mensaje.From = New MailAddress("n.gutierrez777@gmail.com", "NORMAN")
+
+                ' Soporta varios destinatarios separados por ';'
+                For Each destino As String In txtAsunto.Text.Split(";"c)
+                    If Not String.IsNullOrWhiteSpace(destino) Then
+                        mensaje.To.Add(destino.Trim())
+                    End If
+                Next
+
+                mensaje.Subject = txtAsunto.Text
+                mensaje.Body = txtCuerpo.Text
+                mensaje.IsBodyHtml = False
+
+                Using smtp As New SmtpClient("smtp.gmail.com", 587)
+                    smtp.Credentials = New NetworkCredential(
+                        "n.gutierrez777@gmail.com", "yolp jspf hfsr dpll")
+                    smtp.EnableSsl = True
+                    smtp.Send(mensaje)
+                End Using
+            End Using
+
+            MessageBox.Show("Correo enviado correctamente.",
+                "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            LimpiarCampos()
+
+        Catch ex As Exception
+            MessageBox.Show("Ocurrió un error al enviar el correo:" & vbCrLf & ex.Message,
+                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            Me.Cursor = Cursors.Default
+            btnEnviar.Enabled = True
+        End Try
+    End Sub
+
+    Private Sub LimpiarCampos()
+        txtPara.Clear()
+        txtAsunto.Clear()
+        txtCuerpo.Clear()
+        txtPara.Focus()
+    End Sub
+End Class
